75a5f49d3a05af6a25f410d66bef3cd5f997ca38
paudel54 <2sansrit@gmail.com>
2023-12-18 14:43:23 +0545

    bug fix required for deployment db unavailable

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-aeqqwb%4k!d=ntu5&^ps@nwdaf&#6^oac-z&5rfl2re4$o)j2y"
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(filter(None, os.environ.get("ALLOWED_HOSTS", "").split(",")))
 
 
 # Application definition
